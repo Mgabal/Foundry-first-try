6aa4f1bdb464594f95f4e0ec4647c6ecdd98b9e7
Mgabal2021 <98194589+Mgabal@users.noreply.github.com>
2026-02-15 16:14:11 +0200

    Update DeployFundMe script for new deployment logic

--- script/DeployFundMe.s.sol
+++ script/DeployFundMe.s.sol
@@ -1,13 +1,26 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.18;
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.19;
 
 import {Script} from "forge-std/Script.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
 import {FundMe} from "../src/FundMe.sol";
 
-contract DeployFundMe is Script{ 
-    function run() external {
+contract DeployFundMe is Script {
+    function deployFundMe() public returns (FundMe, HelperConfig) {
+        HelperConfig helperConfig = new HelperConfig();
+
+        address priceFeed = helperConfig
+            .getConfigByChainId(block.chainid)
+            .priceFeed;
+
         vm.startBroadcast();
-        new FundMe();
+        FundMe fundMe = new FundMe(priceFeed);
         vm.stopBroadcast();
+
+        return (fundMe, helperConfig);
+    }
+
+    function run() external returns (FundMe, HelperConfig) {
+        return deployFundMe();
     }
 }
