ead0cabe914055a46b0817afa098008f974f66aa
Mgabal2021 <98194589+Mgabal@users.noreply.github.com>
2026-02-15 16:10:53 +0200

    Refactor FundMeTest setup and tests

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -6,27 +6,18 @@ import {FundMe} from "../src/FundMe.sol";
 import {MockV3Aggregator} from "../test/mock/MockV3Aggregator.sol";
 
 contract FundMeTest is Test {
-  FundMe fundMe;
-  function setUp() external {
+    FundMe fundMe;
 
-    fundMe = new FundMe();
-  }
+    function setUp() external {
+        MockV3Aggregator mock = new MockV3Aggregator(8, 2000e8);
+        fundMe = new FundMe(address(mock));
+    }
 
-  function testDemo() public view {
-    assertEq(fundMe.MINIMUM_USD(), 5e18);
-  }
+    function testMinimumUsd() public view {
+        assertEq(fundMe.MINIMUM_USD(), 5e18);
+    }
 
-  function testOwner() public view {
-    console.log("Owner is: ", fundMe.i_owner());
-    console.log("Msg sender is: ", msg.sender);
-    assertEq(fundMe.i_owner(), address(this));
-  }
-
-  function testgestVersion() public view {
-    console.log("Version is: ", fundMe.getVersion());
-    // assertEq(fundMe.getVersion(), 4);
-  }
+    function testOwnerIsMsgSender() public view {
+        assertEq(fundMe.i_owner(), address(this));
+    }
 }
-
-
-// main issue or the reason that the owner is not the same as msg.sender is (we are calling the fundMe test so it deployes the contract and have this access not me)
