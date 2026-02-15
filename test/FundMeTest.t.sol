52bbf34307e4c23d4e85883c8264b2f2edaf621f
Mgabal2021 <98194589+Mgabal@users.noreply.github.com>
2026-02-15 03:51:49 +0200

    Add MockV3Aggregator import to FundMeTest

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.18;
 
 import {Test, console} from "forge-std/Test.sol";
 import {FundMe} from "../src/FundMe.sol";
+import {MockV3Aggregator} from "../test/mock/MockV3Aggregator.sol";
 
 contract FundMeTest is Test {
   FundMe fundMe;
